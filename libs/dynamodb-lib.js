72366416d0d8f4b6dbb373c6079750e73717f227
Charlie Allen <callnan@amazon.com>
2020-12-11 10:51:31 +0000

    Adding APIs

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,13 @@
+import AWS from "aws-sdk";
+
+
+// An convenience object that exposes the DynamoDB client methods.
+const client = new AWS.DynamoDB.DocumentClient();
+
+export default {
+  get: (params) => client.get(params).promise(),
+  put: (params) => client.put(params).promise(),
+  query: (params) => client.query(params).promise(),
+  update: (params) => client.update(params).promise(),
+  delete: (params) => client.delete(params).promise(),
+};
